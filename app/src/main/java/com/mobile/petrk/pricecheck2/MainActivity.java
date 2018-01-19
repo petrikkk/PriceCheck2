5e2d2f96ad49a7b18b77be6c89fa9efb40e4f562
petrikkk <petr.koliba@post.cz>
2018-01-19 12:31:18 +0100

    funkcnost + design

--- app/src/main/java/com/mobile/petrk/pricecheck2/MainActivity.java
+++ app/src/main/java/com/mobile/petrk/pricecheck2/MainActivity.java
@@ -9,6 +9,8 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,30 +22,45 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        DataAPI dataAPI = retrofit.create(DataAPI.class);
-        Call<BtcPrice2> call = dataAPI.getPrice();
-        call.enqueue(new Callback<BtcPrice2>() {
 
-            @Override
-            public void onResponse(Call<BtcPrice2> call, Response<BtcPrice2> response) {
-                BtcPrice2 price = response.body();
-                System.out.println(price.getLast());
-                System.out.println(price.getHigh());
-                System.out.println(price.getLow());
-                TextView zobrazTextView = (TextView)findViewById(R.id.textView);
-                zobrazTextView.setText(price.getLast());
+        Button button = findViewById(R.id.button1);
+         button.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
+                    DataAPI dataAPI = retrofit.create(DataAPI.class);
+                    Call<BtcPrice2> call = dataAPI.getPrice();
+                    call.enqueue(new Callback<BtcPrice2>() {
+                        @Override
+                        public void onResponse(Call<BtcPrice2> call, Response<BtcPrice2> response) {
+                            BtcPrice2 price = response.body();
+                            //testovaci vypisy
+                            System.out.println(price.getLast());
+                            System.out.println(price.getHigh());
+                            System.out.println(price.getLow());
 
-            }
 
-            @Override
-            public void onFailure(Call<BtcPrice2> call, Throwable t) {
-                System.out.println("onFailure");
-                t.printStackTrace();
-            }
-        });
+                            TextView zobrazTextView = (TextView)findViewById(R.id.textView);
+                            zobrazTextView.setText(price.getLast());
+                            TextView zobrazTextView2 = (TextView)findViewById(R.id.textView2);
+                            zobrazTextView2.setText(price.getAsk());
+                            TextView zobrazTextView3 = (TextView)findViewById(R.id.textView3);
+                            zobrazTextView3.setText(price.getBid());
+
+                        }
+
+                        @Override
+                        public void onFailure(Call<BtcPrice2> call, Throwable t) {
+                            System.out.println("onFailure");
+                            t.printStackTrace();
+                        }
+                    });
+                }
+         });
+
+
+
 
     }
 }
