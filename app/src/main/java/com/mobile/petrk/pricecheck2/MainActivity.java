21a9bc1c214c5cccafd8378eb2a68c27edfa6bda
petrikkk <petr.koliba@post.cz>
2018-01-19 15:15:06 +0100

    funkcnost + design

--- app/src/main/java/com/mobile/petrk/pricecheck2/MainActivity.java
+++ app/src/main/java/com/mobile/petrk/pricecheck2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mobile.petrk.pricecheck2;
 
+import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,10 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import android.widget.TextView;
 import android.widget.Button;
 import android.view.View;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -33,22 +38,28 @@ public class MainActivity extends AppCompatActivity {
                     Call<BtcPrice2> call = dataAPI.getPrice();
                     call.enqueue(new Callback<BtcPrice2>() {
                         @Override
+
                         public void onResponse(Call<BtcPrice2> call, Response<BtcPrice2> response) {
                             BtcPrice2 price = response.body();
-                            //testovaci vypisy
+                         //testovaci vypisy
                             System.out.println(price.getLast());
                             System.out.println(price.getHigh());
                             System.out.println(price.getLow());
 
+                         //timestamp
+                            Date date = new Date();
+                            DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                            String timestamp = (dateFormat.format(date));
+                         //seskladani vypisu
+
+                            StringBuilder sb = new StringBuilder();
+                            sb.append("Aktuální cena:" + price.getLast() + "\n Denní minimum" + price.getLow() +
+                             "\n Denní maximum: " + price.getHigh() + "\n Objem: " + price.getVolume() + "\n Čas: " +
+                             timestamp);
 
                             TextView zobrazTextView = (TextView)findViewById(R.id.textView);
-                            zobrazTextView.setText(price.getLast());
-                            TextView zobrazTextView2 = (TextView)findViewById(R.id.textView2);
-                            zobrazTextView2.setText(price.getAsk());
-                            TextView zobrazTextView3 = (TextView)findViewById(R.id.textView3);
-                            zobrazTextView3.setText(price.getBid());
-
-                        }
+                            zobrazTextView.setText(sb.toString());
+                                                   }
 
                         @Override
                         public void onFailure(Call<BtcPrice2> call, Throwable t) {
@@ -56,8 +67,16 @@ public class MainActivity extends AppCompatActivity {
                             t.printStackTrace();
                         }
                     });
+
+
                 }
          });
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                setContentView(R.layout.activity_main2);
+            }
+        });
 
 
 
